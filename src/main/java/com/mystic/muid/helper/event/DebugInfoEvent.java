3c180d39d9f50a3aa7d33a796abdff1997f5ac19
Mysticpasta1 <45608372+Mysticpasta1@users.noreply.github.com>
2020-12-10 09:53:41 -0600

    Updated DebugInfoEvent.java

--- src/main/java/com/mystic/muid/helper/event/DebugInfoEvent.java
+++ src/main/java/com/mystic/muid/helper/event/DebugInfoEvent.java
@@ -1,27 +1,27 @@
 package com.mystic.muid.helper.event;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import static net.minecraft.util.datafix.TypeReferences.BIOME;
+import net.minecraftforge.registries.ForgeRegistries;
 
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
-public class DebugInfoEvent {
+public class DebugInfoEvent{
+
     @SubscribeEvent
     public static void RenderGameOverlayEvent(RenderGameOverlayEvent.Text event) {
         if (Minecraft.getInstance().gameSettings.showDebugInfo) {
-            event.getRight().add("Number of Block IDs Registered: " + Registry.BLOCK.keySet().size());
-            event.getRight().add("Number of Item IDs Registered: " + Registry.ITEM.keySet().size());
-            event.getRight().add("Number of Potion IDs Registered: " + Registry.POTION.keySet().size());
-            event.getRight().add("Number of Enchantment IDs Registered: " + Registry.ENCHANTMENT.keySet().size());
-            event.getRight().add("Number of Fluid IDs Registered: " + Registry.FLUID.keySet().size());
-            event.getRight().add("Number of Effect IDs Registered: " + Registry.EFFECTS.keySet().size());
+            event.getRight().add("Number of Biome IDs Registered: " + ForgeRegistries.BIOMES.getKeys().size());
+            event.getRight().add("Number of Block IDs Registered: " + ForgeRegistries.BLOCKS.getKeys().size());
+            event.getRight().add("Number of Item IDs Registered: " + ForgeRegistries.ITEMS.getKeys().size());
+            event.getRight().add("Number of Potion IDs Registered: " + ForgeRegistries.POTIONS.getKeys().size());
+            event.getRight().add("Number of Enchantment IDs Registered: " + ForgeRegistries.ENCHANTMENTS.getKeys().size());
+            event.getRight().add("Number of Fluid IDs Registered: " + ForgeRegistries.FLUIDS.getKeys().size());
+            event.getRight().add("Number of Tile Entities IDs Registered: " + ForgeRegistries.TILE_ENTITIES.getKeys().size());
+            event.getRight().add("Number of Entity IDs Registered: " + ForgeRegistries.ENTITIES.getKeys().size());
         }
     }
 }
