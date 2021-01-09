8560f8b38494a06bdc9f45fa4dade67da8e60fac
Waterpicker <Waterpickerenternity@gmail.com>
2021-01-09 03:05:19 -0600

    Port to fabric complete

---
+++ src/main/java/com/mystic/muid/event/DebugInfoEvent.java
@@ -0,0 +1,41 @@
+package com.mystic.muid.event;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+
+public class DebugInfoEvent implements HudRenderCallback {
+
+    @Override
+    public void onHudRender(MatrixStack matrixStack, float tickDelta) {
+        if (MinecraftClient.getInstance().options.debugEnabled) {
+            TextRenderer tr = MinecraftClient.getInstance().textRenderer;
+
+            List<String> lines = new ArrayList<>();
+            lines.add("Number of Biome IDs Registered: " + BuiltinRegistries.BIOME.getIds().size());
+            lines.add("Number of Block IDs Registered: " + Registry.BLOCK.getIds().size());
+            lines.add("Number of Item IDs Registered: " + Registry.ITEM.getIds().size());
+            lines.add("Number of Potion IDs Registered: " + Registry.POTION.getIds().size());
+            lines.add("Number of Enchantment IDs Registered: " + Registry.ENCHANTMENT.getIds().size());
+            lines.add("Number of Fluid IDs Registered: " + Registry.FLUID.getIds().size());
+            lines.add("Number of Tile Entities IDs Registered: " + Registry.BLOCK_ENTITY_TYPE.getIds().size());
+            lines.add("Number of Entity IDs Registered: " + Registry.ENTITY_TYPE.getIds().size());
+
+            matrixStack.translate(MinecraftClient.getInstance().getWindow().getScaledWidth(),MinecraftClient.getInstance().getWindow().getScaledHeight() - (lines.size() * tr.fontHeight),0);
+
+            for (int i = 0; i < lines.size(); i++) {
+                String line = lines.get(i);
+
+                tr.draw(matrixStack, line, -tr.getWidth(line),i * tr.fontHeight, 14737632);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/mystic/muid/util/Reference.java
@@ -0,0 +1,9 @@
+package com.mystic.muid.util;
+
+public class Reference {
+    public static final String MODID = "muid";
+    public static final String NAME = "MUID";
+    public static final String VERSION = "1.2.0-12-12-2020-1.16.4-forge";
+    public static final String ACCEPTED_VERSION = "[1.16.4, 1.17)";
+
+}
